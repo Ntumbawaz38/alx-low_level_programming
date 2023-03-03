20846d99cf611b2b2dd06597ce6c4fa470b1790a
Ntumba wa Ntumba <ntumbawaz@gmail.com>
2023-03-03 13:07:08 -0800

    first commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,24 +1,19 @@
 #include "main.h"
 /**
- * _strcat - concatenates two strings
- * @dest: dest string
- * @src: src string
- * Return: pointer to resulting string
+ * *_strcat - concatenates @src to @dest
+ * @src: the source string to append to @dest
+ * @dest: the destiation string to be concatenated upon
+ * Return:pointer to the resulting string
  */
+
 char *_strcat(char *dest, char *src)
 {
-	int s = 0, i = 0;
+	int index = 0;
+	int dest_len = 0;
 
-	while (dest[s] != 0)
-	{
-		s++;
-	}
-	while (src[i] != 0)
-	{
-		dest[s + i] = src[i];
-		i++;
-	}
-
-	dest[s + i] = 0;
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
 	return (dest);
 }
